81d7eae7372257a05fd6f9aaedc0c6b7fa641035
Abhi_Joshi <joshi.abhiram98@gmail.com>
2025-01-06 01:19:17 +0000

    Set default shell by OS

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.number = true
 
+local uname = vim.loop.os_uname().sysname
+if uname == "Windows_NT" then
+  vim.opt.shell = "powershell.exe"
+elseif uname == "Darwin" then
+  vim.opt.shell = "zsh"
+else
+  vim.opt.shell = "bash"
+end

92f6075b9dc65de28ff67a99ec0224e1dd92a1d5
Abhi Joshi <joshi.abhirma98@gmail.com>
2025-01-22 17:14:10 +0000

    Remove Github CI conf

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "ruff", "ts_ls", "gopls", "gh_actions_ls", "yamlls" },
+        ensure_installed = { "lua_ls", "pyright", "ruff", "ts_ls", "gopls", "yamlls" },
       })
     end,
   },

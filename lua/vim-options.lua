811c466d0c95547234e175309255143087708c80
Abhi Joshi <joshi.abhirma98@gmail.com>
2025-02-19 00:03:34 +0000

    Add keymaps for home and end

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,7 +13,14 @@ else
   vim.opt.shell = "bash"
 end
 
-
-vim.keymap.set('n', '<C-w>z', ':setlocal spell spelllang=en_ca', { desc = "Enable spellcheck"})
-vim.keymap.set('n', '<C-w>d', vim.diagnostic.open_float, { desc = "Show error dropdown"})
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]],{noremap=true})
+-- Enable home and end keys to go to start and end of line
+vim.keymap.set('n', '<Home>', '^', { noremap = true, silent = true })
+vim.keymap.set('n', '<End>', 'g_', { noremap = true, silent = true })
+vim.keymap.set('i', '<Home>', '<C-o>^', { noremap = true, silent = true })
+vim.keymap.set('i', '<End>', '<C-o>g_', { noremap = true, silent = true })
+-- Enable spellcheck
+vim.keymap.set('n', '<C-w>z', ':setlocal spell spelllang=en_ca', { desc = "Enable spellcheck" })
+-- Expand error and warning dropdowns
+vim.keymap.set('n', '<C-w>d', vim.diagnostic.open_float, { desc = "Show error dropdown" })
+-- Map escape to terminal defocus
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
